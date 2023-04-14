42c1f0a036ac14b36ce7c70f576d540e8c3b814b
Stef Piatek <s.piatek@ucl.ac.uk>
2023-04-14 16:34:52 +0100

    Pass pylint

--- src/calendar_bouncer/delete_permissions.py
+++ src/calendar_bouncer/delete_permissions.py
@@ -3,14 +3,19 @@ from loguru import logger
 
 from calendar_bouncer.config import credential, scopes
 
+REQUEST_TIMEOUT_SECONDS = 10
+
 
 def delete_permissions():
+    """Delete all shared permissions on all calendars where that permission is removable (using REST API)"""
     access = credential.get_token(" ".join(scopes))
     headers = {"Authorization": f"Bearer {access.token}", "Accept": "application/json"}
     permissions_url = (
         "https://graph.microsoft.com/v1.0/me/calendar/calendarPermissions/"
     )
-    response = requests.get(permissions_url, headers=headers)
+    response = requests.get(
+        permissions_url, headers=headers, timeout=REQUEST_TIMEOUT_SECONDS
+    )
     shared_calendars = response.json().get("value")
     for calendar in shared_calendars:
         if calendar.get("isRemovable") is True:
@@ -19,6 +24,7 @@ def delete_permissions():
 
 
 def delete_shared_calendar(calendar, headers):
+    """Delete a single shared calendar via REST API"""
     delete_calendar_template = (
         "https://graph.microsoft.com/v1.0/me/calendar/calendarPermissions/{calendar_id}"
     )
@@ -28,5 +34,7 @@ def delete_shared_calendar(calendar, headers):
         shared_with=calendar.get("emailAddress").get("name"),
     )
     requests.delete(
-        delete_calendar_template.format(calendar_id=calendar_id), headers=headers
+        delete_calendar_template.format(calendar_id=calendar_id),
+        headers=headers,
+        timeout=REQUEST_TIMEOUT_SECONDS,
     )
--- src/calendar_bouncer/sdk.py
+++ src/calendar_bouncer/sdk.py
@@ -6,15 +6,23 @@ from loguru import logger
 from msgraph.generated.me.calendar.calendar_permissions.calendar_permissions_request_builder import (
     CalendarPermissionsRequestBuilder,
 )
+from msgraph.generated.models.o_data_errors.o_data_error import ODataError
 
 from calendar_bouncer.config import client
 
 
 async def delete_calendar_permissions_using_sdk():
+    """
+    Delete calendar permissions using SDK API
+
+    Currently, failing because it looks like there are no permissions and also get this error:
+
+    > The following parameters are not supported with change tracking over the 'FindCalendarPermissions' resource:
+    > '$orderby, $filter, $select, $expand, $search, $delta'
+
+    """
     user = await client.users_by_id("sejjpia@ucl.ac.uk").get()
-    me = await client.me.get()
     logger.debug(user)
-    logger.debug(me)
 
     query_params = CalendarPermissionsRequestBuilder.CalendarPermissionsRequestBuilderGetQueryParameters(
         select=["isRemovable"]
@@ -27,19 +35,21 @@ async def delete_calendar_permissions_using_sdk():
     )
     logger.debug("Number of permissions: {count}", count=permission_count)
 
+    message_template = "Error: {message}"
     try:
         permission = await client.me.calendar.calendar_permissions.get(request_config)
         logger.success(permission)
-    except Exception as e:
-        message_template = "Error: {message}"
-        if e.__getattribute__("error"):
-            logger.error(message_template, message=e.error.message)
-        else:
-            logger.error(message_template, message=e)
-
-        raise e
+    except ODataError as exception:
+        logger.error(message_template, message=exception.error.message)
+    except Exception as exception:
+        logger.error(message_template, message=exception)
 
 
 def delete_permissions():
+    """Wrap async call in function"""
     asyncio.run(delete_calendar_permissions_using_sdk())
     logger.success("All done")
+
+
+if __name__ == "__main__":
+    delete_permissions()
